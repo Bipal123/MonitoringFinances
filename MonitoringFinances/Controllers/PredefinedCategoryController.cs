d246d3d0d5c44fc75ea0fad8fc5fa8d27b0ed039
Bipal Goyal <goyalbl@mail.uc.edu>
2021-10-18 01:30:34 +0545

    Create PreDefinedCategory Index method and View

---
+++ MonitoringFinances/Controllers/PredefinedCategoryController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MonitoringFinances.Data;
+using MonitoringFinances.Models.AdminModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MonitoringFinances.Controllers
+{
+    public class PredefinedCategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public PredefinedCategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<PredefinedCategory> predefinedCategoryList = _db.PredefinedCategory;
+            return View(predefinedCategoryList);
+        }
+
+        ////Get - Insert and Update
+        //public IActionResult UpSert()
+        //{
+
+        //}
+    }
+}
