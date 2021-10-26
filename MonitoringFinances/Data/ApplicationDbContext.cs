2e76aaa486d2dc981fe1e6fcccb63b9dccbfd23a
Bipal Goyal <goyalbl@mail.uc.edu>
2021-10-26 22:12:04 +0545

    Added Category to Database with Migration

--- MonitoringFinances/Data/ApplicationDbContext.cs
+++ MonitoringFinances/Data/ApplicationDbContext.cs
@@ -17,6 +17,7 @@ namespace MonitoringFinances.Data
 
         }
         public DbSet<ApplicationUser> ApplicationUser { get; set; }
-        public DbSet<PredefinedCategory> PredefinedCategory { get; set; } 
+        public DbSet<PredefinedCategory> PredefinedCategory { get; set; }
+        public DbSet<Category> Category { get; set; }
     }
 }
---
+++ MonitoringFinances/Migrations/20211026162411_addCategory.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MonitoringFinances.Migrations
+{
+    public partial class addCategory : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Category",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Category", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Category_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Category_UserId",
+                table: "Category",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Category");
+        }
+    }
+}
