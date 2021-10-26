febf8ffb3d6c040812f50521ed9fa27330bb818b
Bipal Goyal <goyalbl@mail.uc.edu>
2021-10-26 22:11:45 +0545

    Add Category Model

---
+++ MonitoringFinances/Models/Category.cs
@@ -0,0 +1,21 @@
+﻿using MonitoringFinances.Models.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MonitoringFinances.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string UserId { get; set; }
+        [ForeignKey("UserId")]
+        public virtual ApplicationUser ApplicationUser { get; set; }
+    }
+}
