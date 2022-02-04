01dae71b2b9d06fe6102c52dc1ea83a3c6caa290
Bipal Goyal <goyalbl@mail.uc.edu>
2022-02-04 17:49:44 -0500

    cards on charts, minor changes

--- MonitoringFinances/Startup.cs
+++ MonitoringFinances/Startup.cs
@@ -38,17 +38,16 @@ namespace MonitoringFinances
         {
             Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("NTY0MTE5QDMxMzkyZTM0MmUzMEE1OTM4R01nU0dQYmNRV01tWVNsTFRtUDM2VUNOcnh0ODM2dWtXTnVvcTg9");
 
-            //if (env.IsDevelopment())
-            //{
-            //    app.UseDeveloperExceptionPage();
-            //}
-            //else
-            //{
-            //    app.UseExceptionHandler("/Home/Error");
-            //    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-            //    app.UseHsts();
-            //}
-            app.UseDeveloperExceptionPage();
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
